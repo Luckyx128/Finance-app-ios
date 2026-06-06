50366f4df2bd4466fdb714739ff18165b43aacbd
Lucas Amorim <lucas.amorim@ambiensys.com.br>
2026-06-06 16:52:41 -0300

    Initial Commit

---
+++ financeTests/financeTests.swift
@@ -0,0 +1,17 @@
+//
+//  financeTests.swift
+//  financeTests
+//
+//  Created by Lucas de Amorim on 06/06/26.
+//
+
+import Testing
+@testable import finance
+
+struct financeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
